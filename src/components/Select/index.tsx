cdea26b2e3096043d44f2eb4d6bf0931e583c4d0
HenriqueContini <henrique.coutine@gmail.com>
2023-05-18 12:34:08 -0300

    Chore: Adding clear input button to Selects

--- src/components/Select/index.tsx
+++ src/components/Select/index.tsx
@@ -1,5 +1,6 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './Select.module.css'
+import { IoMdClose } from 'react-icons/io'
 
 interface SelectProps {
   children: React.ReactNode
@@ -29,7 +30,15 @@ export default function Select({ children, name, label, placeholder, filterFunct
   return (
     <fieldset className={styles.fieldset}>
       <label htmlFor={name} className={styles.label}>{label}</label>
-      <input type='search' name={name} className={styles.input} value={inputValue} placeholder={placeholder} onChange={(e) => handleChange(e)} />
+      <div className={styles.input__container}>
+        <input type='search' name={name} className={styles.input} value={inputValue} placeholder={placeholder} onChange={(e) => handleChange(e)} />
+        <div className={styles.clearInput} onClick={() => {
+          setInputValue('')
+          setSelectedValue('')
+        }}>
+          <IoMdClose/>
+        </div>
+      </div>
 
       {!selectedValue ?
         <ul>
