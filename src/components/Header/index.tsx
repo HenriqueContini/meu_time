2d0ea37a3f4d886f8dfeb832d417a76fefb21dab
HenriqueContini <henrique.coutine@gmail.com>
2023-05-19 14:19:57 -0300

    Refactor: New Header with Nav

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,17 @@
 import styles from './Header.module.css'
+import logo from '../../assets/meuTime__logo.png'
+import { Link } from 'react-router-dom'
 
 export default function Header() {
   return (
     <header className={styles.header}>
-      <img src="meuTime__logo.png" alt="Logo Meu time" className={styles.img}/>
+      <div className={styles.title}>
+        <img src={logo} alt="Logo Meu time" className={styles.img} />
+        <h1>Meu time</h1>
+      </div>
+      <nav className={styles.nav}>
+        <Link to='/home' className={styles.link}>Início</Link>
+      </nav>
     </header>
   )
 }
